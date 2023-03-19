1f316b08b806d8b89c41ae7ee472570f000abd93
Bianca Power <bianca@biancapower.com>
2023-03-19 16:57:12 +1100

    Initial commit bundle gem open_library

---
+++ spec/open_library_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe OpenLibrary do
+  it "has a version number" do
+    expect(OpenLibrary::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
